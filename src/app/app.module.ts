d0923d33fb1b5b1755f8362a842fd96ccb5fcfea
aruizapex <aruiz@apexsystems.com>
2024-04-16 18:04:38 -0600

    trabajar con directivas estructurales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {MatTabsModule} from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectivasDeAtributoComponent } from './components/directivas-de-atributo/directivas-de-atributo.component';
+import { DirectivasEstructuralesComponent } from './components/directivas-estructurales/directivas-estructurales.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivasDeAtributoComponent
+    DirectivasDeAtributoComponent,
+    DirectivasEstructuralesComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/directivas-estructurales/directivas-estructurales.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directivas-estructurales',
+  templateUrl: './directivas-estructurales.component.html',
+  styleUrl: './directivas-estructurales.component.scss'
+})
+export class DirectivasEstructuralesComponent {
+  array: string[] = ['A', 'B', 'C'];
+}
