3a559abb4918f75097c7145fe750a408f47801dc
aruizapex <aruiz@apexsystems.com>
2024-04-25 09:01:11 -0600

    create MyFirstModule module and component, integrate them with app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { DirectivasDeAtributoComponent } from './components/directivas-de-atribu
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { DirectivasDeAtributo2Component } from './components/directivas-de-atributo-2/directivas-de-atributo-2.component';
 import {MatTabsModule} from '@angular/material/tabs';
-
+import {MyFirstModuleModule} from './modules/my-first-module/my-first-module.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +17,8 @@ import {MatTabsModule} from '@angular/material/tabs';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTabsModule
+    MatTabsModule,
+    MyFirstModuleModule
   ],
   providers: [
     provideClientHydration(),
---
+++ src/app/modules/my-first-module/my-first-module.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyFirstComponentComponent } from '@components/my-first-component/my-first-component.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [{
+    path: '',
+    component: MyFirstComponentComponent,
+  }]
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class MyFirstModuleModule { }
